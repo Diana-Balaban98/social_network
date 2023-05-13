fdda999a46986e434b30546b80411ed0644d1e90
Diana Balaban <diana.rud.2012@mail.ru>
2023-05-13 23:55:19 +0300

    route

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,32 +1,38 @@
 import React from "react";
 import s from "./Navbar.module.css";
-// {`${s.item} ${s.active}`} синтаксис для задания более одного класса элементу
-
-const NavMenu = () => {
-    return (
-        <nav className="nav">
-            <div>
-                <a className={s.item} href="#">Profile</a>
-            </div>
-            <div>
-                <a className={s.item} href="#">Messages</a>
-            </div>
-            <div>
-                <a className={s.item} href="#">News</a>
-            </div>
-            <div>
-                <a className={s.item} href="#">Music</a>
-            </div>
-            <div>
-                <a className={s.item} href="#">Settings</a>
-            </div>
-        </nav>)
-}
 
 
 const Navbar = () => {
     return (
-        <NavMenu/>
+        <nav className="nav">
+            <div>
+                <a
+                    className={s.item}
+                    href="/profile">Profile
+                </a>
+            </div>
+            <div>
+                <a
+                    className={s.item}
+                    href="/dialogs">Messages
+                </a>
+            </div>
+            <div>
+                <a
+                    className={s.item} >News
+                </a>
+            </div>
+            <div>
+                <a
+                    className={s.item}>Music
+                </a>
+            </div>
+            <div>
+                <a
+                    className={s.item}>Settings
+                </a>
+            </div>
+        </nav>
     )
 }
 
